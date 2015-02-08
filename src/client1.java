7b04d649e1666411258ff321eebb5a799b410634
Qingxiang Jia <ybjqx3340@gmail.com>
2015-02-08 14:35:21 -0500

    current progress now tested

--- src/client1.java
+++ src/client1.java
@@ -1,9 +1,8 @@
 import javax.crypto.*;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.KeySpec;
@@ -12,7 +11,9 @@ public class client1
 {
     public void execMission(String pwd, String fileName, String serverIP, String port, String RSA)
     {
-
+        byte[] file = readFile(fileName);
+        byte[] cipherText = enryptAES(pwd.getBytes(Charset.forName("UTF-8")), file);
+        System.out.printf("size of file: %d   size of cipher text: %d\n", file.length, cipherText.length);
     }
 
     private byte[] enryptAES(byte[] pwd, byte[] file)
@@ -46,19 +47,29 @@ public class client1
     private byte[] readFile(String filename)
     {
         File file = new File(filename);
-        FileInputStream fileInputStream = null;
-        byte[] bFile = new byte[(int) file.length()];
+        FileInputStream stream = null;
+        byte[] fileInBytes = new byte[(int) file.length()];
         try
         {
-            fileInputStream = new FileInputStream(file);
-            fileInputStream.read(bFile);
-            fileInputStream.close();
-        }
-        catch (Exception e)
+            stream = new FileInputStream(file);
+            int count = stream.read(fileInBytes);
+            System.out.println("Read in " + count + " bytes");
+            stream.close();
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
         {
             e.printStackTrace();
         }
-        return bFile;
+        return fileInBytes;
+    }
+
+    // quick test
+    public static void main(String[] args)
+    {
+        client1 c1 = new client1();
+        c1.execMission("1234567887654321", "liukanshan.jpg", "10.0.0.1", "8087", "RSA");
     }
 
 }

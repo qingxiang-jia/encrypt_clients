6fcf5b9ae1709f08bb2c40c020fbc6e59712c732
Qingxiang Jia <ybjqx3340@gmail.com>
2015-02-18 15:56:45 -0500

    commented client2.java

--- src/client2.java
+++ src/client2.java
@@ -1,4 +1,3 @@
-import javax.crypto.IllegalBlockSizeException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.InetSocketAddress;
@@ -8,35 +7,37 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Set;
 
-// client2 listens to port2 for incoming file
+/**
+ * client2 listens to port2 for incoming file, decrypts the file, checks for validity
+ */
 public class client2
 {
-    boolean shouldRun;
+    boolean shouldRun; // set to true to run
     public void run(int port2)
     {
         try
         {
-            ServerSocketChannel c22s = ServerSocketChannel.open();
-            c22s.socket().bind(new InetSocketAddress(port2));
-            c22s.configureBlocking(false);
+            ServerSocketChannel c22s = ServerSocketChannel.open(); // channel connects client2 to server (c22s)
+            c22s.socket().bind(new InetSocketAddress(port2)); // client2 listens on port2
+            c22s.configureBlocking(false); // use selector, so non-blocking
 
             Selector selector = Selector.open();
-            SelectionKey fromS = c22s.register(selector, SelectionKey.OP_ACCEPT);
+            SelectionKey fromS = c22s.register(selector, SelectionKey.OP_ACCEPT); // accept data from server
 
-            while (shouldRun)
+            while (shouldRun) // client2 also acts like a server
             {
-                int readyChannels = selector.select();
+                int readyChannels = selector.select(); // select channels that are ready
                 if (readyChannels == 0) // no one contacts, keep looping
                     continue;
 
-                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys(); // get a set of client to serve
                 Iterator<SelectionKey> iter = selectedKeys.iterator();
                 while (iter.hasNext()) // serve each client one after another
                 {
                     SelectionKey key = iter.next();
-                    System.out.println("got data from client1");
+                    System.out.println("got data from server"); // server is the client
                     if (key == fromS)
-                        validation(key.channel());
+                        validation(key.channel()); // if is from server, validate the packet
                 }
                 iter.remove(); // this client is served, remove it from the set
             }
@@ -46,26 +47,30 @@ public class client2
         }
     }
 
+    /**
+     * Validates the contents from server.
+     * @param selectableChannel Channel connects server and client2
+     */
     // order: byte[] ePwd, byte[] cipherText, byte[] eHash
     private void validation(SelectableChannel selectableChannel)
     {
-        SocketChannel toClient1 = null;
+        SocketChannel toServer = null; // channel connects server and client2
         ObjectInputStream ois = null;
         try
         {
-            toClient1 = ((ServerSocketChannel) selectableChannel).accept();
-            ois = new ObjectInputStream(toClient1.socket().getInputStream());
+            toServer = ((ServerSocketChannel) selectableChannel).accept();
+            ois = new ObjectInputStream(toServer.socket().getInputStream()); // get data from server
             Cargo bundle = (Cargo) ois.readObject();
             System.out.printf("ePwd size: %d   cipherText size: %d   eHash size: %d\n", bundle.ePwd.length, bundle.cipherText.length, bundle.eHash.length);
 
-            PublicKey c1p = (PublicKey) Util.deserialize("c1p.ser");
-            byte[] hash = Crypto.decryptRSA(bundle.eHash, c1p);
-            byte[] pwd = Crypto.decryptRSA(bundle.ePwd, c1p);
-            byte[] file = Crypto.decryptAES(pwd, bundle.cipherText);
-            byte[] dHash = null;
+            PublicKey c1p = (PublicKey) Util.deserialize("c1p.ser"); // client2 has client1's public key
+            byte[] hash = Crypto.decryptRSA(bundle.eHash, c1p); // get the hash of the decrypted file
+            byte[] pwd = Crypto.decryptRSA(bundle.ePwd, c1p); // get the decrypted password
+            byte[] file = Crypto.decryptAES(pwd, bundle.cipherText); // get the decrypted file
+            byte[] dHash = null; // decrypted hash of the file
             if (file != null)
-                dHash = Crypto.getHash(file);
-            if (dHash != null && Arrays.equals(hash, dHash))
+                dHash = Crypto.getHash(file); // if file swapped, file will be null since decryption failed
+            if (dHash != null && Arrays.equals(hash, dHash)) // if dHash == hash, file is not swapped
             {
                 System.out.println("Verification Passed");
                 Util.writeFile(file, "client2data");
@@ -85,8 +90,8 @@ public class client2
             {
                 if (ois != null)
                     ois.close();
-                if (toClient1 != null)
-                    toClient1.close();
+                if (toServer != null)
+                    toServer.close();
             } catch (IOException e)
             {
                 e.printStackTrace();
@@ -100,6 +105,5 @@ public class client2
         c2.shouldRun = true;
         c2.run(Integer.parseInt(args[0]));
     }
-
 
 }
